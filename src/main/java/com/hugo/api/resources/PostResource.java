030b170700aee874ba7465557b5b2dde87b28fed
Hugo Pablo <hugosampaio@prodata.inf.br>
2021-08-20 20:48:09 -0300

    Avanço 2

--- src/main/java/com/hugo/api/resources/PostResource.java
+++ src/main/java/com/hugo/api/resources/PostResource.java
@@ -5,10 +5,13 @@ import com.hugo.api.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,16 +22,47 @@ public class PostResource {
 
     @Autowired
     private PostService service;
+    private Integer id;
+    private Integer action;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> findAll(){
         List<Post> posts = service.findAll();
+
         return ResponseEntity.ok().body(posts);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> findOne(@PathVariable Integer id){
-        Post post =  service.findOne(id);
+        Post post = service.findOne(id);
+
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Post post){
+        post = service.insert(post);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Post post, @PathVariable Integer id){
+        post.setId(id);
+        service.update(post);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/{action}", method = RequestMethod.POST)
+    public ResponseEntity<Void> upDown(@PathVariable Integer id, @PathVariable Integer action){
+        if(action == 1) {
+            service.up(id);
+        }else {
+            service.down(id);
+        }
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/hugo/api/services/PostService.java
+++ src/main/java/com/hugo/api/services/PostService.java
@@ -2,6 +2,7 @@ package com.hugo.api.services;
 
 import com.hugo.api.domains.Post;
 import com.hugo.api.repositories.PostRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +15,38 @@ public class PostService {
     @Autowired
     private PostRepository repository;
 
-    public Post findOne(Integer id){
+    public Post findOne(Integer id) throws ObjectNotFoundException {
         Optional<Post> post = repository.findById(id);
+        if(post == null)
+            throw new ObjectNotFoundException("Post Não Encontrado Id: " + id, Post.class.getName());
         return post.orElse(null);
     }
 
     public List<Post> findAll(){
         return repository.findAll();
+    }
+
+    public Post insert(Post post){
+        post.setId(null);
+        return repository.save(post);
+    }
+
+    public Post update(Post post){
+        findOne(post.getId());
+        return repository.save(post);
+    }
+
+    public Post up(Integer id){
+        Post post = findOne(id);
+        //Integer up = (post.getUp() | 0) + 1;
+        post.setUp(1);
+        return repository.save(post);
+    }
+
+    public Post down(Integer id){
+        Post post = findOne(id);
+        //Integer down = (post.getDown() | 0) + 1;
+        post.setDown(1);
+        return repository.save(post);
     }
 }
